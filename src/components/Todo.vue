65d0c6a60395cbbe5ecb29a18a2eaaf111e75113
julian <martins_julian@gmx.de>
2022-10-06 14:37:16 +0200

    added tailwind

---
+++ src/components/Todo.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    <h1>{{message.toLowerCase()}}</h1>
+    <h1>{{count}}</h1>
+    <button @click="countUp">Hochzählen</button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Todo',
+  data(){
+    return {
+      message: "Hello World",
+      count: 0,
+    }
+  },
+  methods: {
+    countUp(){
+      this.count++;
+    }
+  }
+}
+</script>
