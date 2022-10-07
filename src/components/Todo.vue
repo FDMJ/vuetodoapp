17b2d68f47fe50044b3f88a3a4a2ddf505b6fe9f
julian <martins_julian@gmx.de>
2022-10-07 08:45:33 +0200

    finished app

--- src/components/Todo.vue
+++ src/components/Todo.vue
@@ -1,24 +1,28 @@
 <template>
-  <div>
-    <h1>{{message.toLowerCase()}}</h1>
-    <h1>{{count}}</h1>
-    <button @click="countUp">Hochzählen</button>
+  <div class="flex justify-between items-center py-2 px-2" 
+      :class="{'bg-green-600': this.todo.done, 'bg-red-600': !this.todo.done }"
+  >
+    <h1 class="cursor-pointer" @click="toggleTodo">{{ todo.todo }}</h1>
+    <button class="bg-black py-1 px-1 rounded" @click="removeTodo">Delete Todo</button>
   </div>
 </template>
 
 <script>
 export default {
   name: 'Todo',
+  props: ["todoprop", "todoindex"],
   data(){
     return {
-      message: "Hello World",
-      count: 0,
-    }
+      todo: this.todoprop,
+    };
   },
   methods: {
-    countUp(){
-      this.count++;
+    toggleTodo(){
+      this.$emit("toggledone-index", this.todoindex);
+    },
+    removeTodo(){
+      this.$emit("removetodo-index", this.todoindex); 
     }
-  }
-}
+  },
+};
 </script>
